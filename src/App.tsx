a0de0819e04c14db77fabc51eaa8fa4159c63ed4
Aditya <adityasingh220598@gmail.com>
2022-07-03 14:20:47 +0530

    The context API and typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
-import { useContext } from "react";
 import "./App.css";
 import NewTodo from "./components/NewTodo";
 import Todos from "./components/Todos";
-import { TodosContext } from "./store/todos-context";
 
 function App() {
-  const todoCtx = useContext(TodosContext);
   return (
     <>
       <NewTodo />
-      <Todos todos={todoCtx.todos} />;
+      <Todos />;
     </>
   );
 }
--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,11 +1,13 @@
-import { Todo } from "../models/Todo";
+import { useContext } from "react";
+import { TodosContext } from "../store/todos-context";
 import TodoItem from "./TodoItem";
 import classes from "./Todos.module.css";
 
-const Todos: React.FC<{ todos: Todo[] }> = ({ todos }) => {
+const Todos: React.FC = () => {
+  const todosCtx = useContext(TodosContext);
   return (
     <ul className={classes.todos}>
-      {todos.map((todo) => (
+      {todosCtx.todos.map((todo) => (
         <TodoItem key={todo.id} todo={todo} />
       ))}
     </ul>
