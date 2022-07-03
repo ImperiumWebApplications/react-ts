751a1ea72a80bcfba97bcfa9ac6eff569a9e97b3
Aditya <adityasingh220598@gmail.com>
2022-07-03 09:03:20 +0530

    Working with props and typescipt

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,10 +1,19 @@
-function Todos() {
+// todos is an array of objects with the following properties:
+// id: number
+// text: string
+type Todo = {
+  id: number;
+  text: string;
+};
+
+const Todos: React.FC<{ todos: Todo[] }> = ({ todos }) => {
   return (
     <ul>
-      <li>Todo 1</li>
-      <li>Todo 2</li>
+      {todos.map((todo) => (
+        <li key={todo.id}>{todo.text}</li>
+      ))}
     </ul>
   );
-}
+};
 
 export default Todos;
